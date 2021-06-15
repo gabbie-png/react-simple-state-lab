73c481244c2eb027efea3962d76fbeeb3e1f4be3
Gabriella RoseMarino <Gabriellarosemarino@gmail.com>
2021-06-15 19:39:50 -0400

    commit tree 0f1f478e6b9962776f5ce440a7b628e7fdbdd2e6

---
+++ src/Cell.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+export default class Cell extends React.Component{
+    constructor(props) {
+        super()
+        this.state = {color: props.value} // ...define initial state with a key of 'color' set to the 'value' prop
+      } 
+      handleClick = () => {
+          this.setState({color: "#333"})
+      }
+    render(){
+        return <div onClick = {this.handleClick} className='cell' style={{backgroundColor: this.state.color}}></div>
+    }
+}
+
+//Cell.addEventListener('click',this.setState())
